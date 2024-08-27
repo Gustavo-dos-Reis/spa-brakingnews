b0e37dc879ef23623f2835c74e57dcaff20ce59a
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2024-08-27 16:58:17 -0300

    feat: create component navbar, add bootstrap icons

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components/Navbar/Navabar";
+
+export default function Home(){
+    return (
+        <>
+            <Navbar/>
+            <h1>Ola Home</h1>
+        </>
+    )
+}
