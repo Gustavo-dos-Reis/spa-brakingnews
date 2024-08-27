fe500648bcb1e81e6ea158678930f3e0181c27af
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2024-08-27 09:54:38 -0300

    feat: creat data mock

---
+++ src/Datas.js
@@ -0,0 +1,30 @@
+export const news = [
+    {
+        title: "Noticia 1",
+        text: "Lorem",
+        Image: "https://www.solopress.com/blog/wp-content/uploads/2020/11/Lorem-Ipsum.jpg",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Noticia 2",
+        text: "Lorem",
+        Image: "https://www.solopress.com/blog/wp-content/uploads/2020/11/Lorem-Ipsum.jpg",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Noticia 3",
+        text: "Lorem",
+        Image: "https://www.solopress.com/blog/wp-content/uploads/2020/11/Lorem-Ipsum.jpg",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Noticia 4",
+        text: "Lorem",
+        Image: "https://www.solopress.com/blog/wp-content/uploads/2020/11/Lorem-Ipsum.jpg",
+        likes: 2,
+        comments: 4,
+    },
+]
